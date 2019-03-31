37c36880252a9a84b99f011c0b3a599a2aa51351
root <root@localhost.localdomain>
2019-03-31 05:41:51 -0400

    Updated ceaser cipher function

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,74 +1,64 @@
-# Caesar Cipher
+
+def encrypt(string2,key):
+	result = ""
+	
+	for i in range(len(string2)):
+		char=string2[i]
+		#print(char)
+
+		if(char.isupper()):
+			result=result+chr((ord(char) + key - 65 ) % 26 + 65 )
+		if(char.islower()):
+			result=result+chr((ord(char) + key - 97 ) % 26 + 97 )
+	return result
+
+def decrypt(cipher,key):
+	result=""
+
+	for i in range(len(cipher)):
+		char=cipher[i]
+		
+		if(char.isupper()):
+			result=result+chr((ord(char) - key - 65 ) % 26 + 65)
+		if(char.islower()):
+			result=result+chr((ord(char) - key - 97 ) % 26 + 97 )
+	return result
+
+def bruteforce(cipher):
+	
+	for key in range(0,26):
+		result=""
+		for i in range(len(cipher)):
+			char=cipher[i]
+		
+			if(char.isupper()):
+				result=result+chr((ord(char) - key - 65 ) % 26 + 65)
+			if(char.islower()):
+				result=result+chr((ord(char) - key - 97 ) % 26 + 97 )
+		
+		print("Using Key #",key,": ",result)
 
 
+print('''Enter Choice:
+	1:Encrypt
+	2:Decrypt
+	3:Brute Force
+	Other key:Exit''')
+ch=int(input())
+if(ch==1):
+	string1=input("Text:")
+	key=int(input("Shift Key:"))
+	cipher=encrypt(string1,key)	
+	print("cipher:",cipher)
 
-MAX_KEY_SIZE = 26
-
-
-
-def getMode():
-     while True:
-	print('Do you wish to encrypt or decrypt a message?')
-        mode = input().lower()
-	if mode in 'encrypt e decrypt d'.split():
-             return mode
-         else:
-             print('Enter either "encrypt" or "e" or "decrypt" or "d".')
-
-
-
-def getMessage():
-     print('Enter your message:')
-     return input()
-
-
-
-def getKey():
-     key = 0
-     while True:
-         print('Enter the key number (1-%s)' % (MAX_KEY_SIZE))
-         key = int(input())
-
-         if (key >= 1 and key <= MAX_KEY_SIZE):
-             return key
-
-
-
-def getTranslatedMessage(mode, message, key):
-     if mode[0] == 'd':
-         key = -key
-     translated = ''
-     
-     for symbol in message:
-         if symbol.isalpha():
-             num = ord(symbol)
-             num += key
-
-
-             if symbol.isupper():
-                 if num > ord('Z'):
-                     num -= 26
-                 elif num < ord('A'):
-                     num += 26
-             elif symbol.islower():
-                 if num > ord('z'):
-                     num -= 26
-                 elif num < ord('a'):
-                     num += 26
-
-
-             translated += chr(num)
-         else:
-             translated += symbol
-     return translated 	
-
-
-
-mode = getMode()
-message = getMessage()
-key = getKey()
-
-
-print('Your translated text is:')
-print(getTranslatedMessage(mode, message, key))
+elif(ch==2):
+	cipher=input("Cipher Text:")
+	key=int(input("Shift Key:"))
+	plaintext=decrypt(cipher,key)
+	print("Plaintext:",plaintext)
+elif(ch==3):
+	cipher=input("Cipher Text:")
+	bruteforce(cipher)
+else:
+	exit(0)
 
